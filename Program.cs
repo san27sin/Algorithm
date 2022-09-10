b9641416b11fbff9d28c9c5284cd3e04ac411b6f
san27sin <sin27san@gmail.com>
2022-09-10 11:53:11 +0300

    Для прямоугольного поля размера M на N клеток, подсчитать количество путей из верхней левой клетки в правую нижнюю. Известно что ходить можно только на одну клетку вправо или вниз.

--- Program.cs
+++ Program.cs
@@ -3,10 +3,71 @@
 namespace Algorithm
 {
     class Program
-    {
+    {       
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Task_1(); 
+        }
+
+        private static void Task_1()
+        {            
+            Console.WriteLine("Введите размер прямоугольника в ширину и длину:");
+            int m = 0;
+            int n = 0;
+
+            do
+            {
+              Console.Write("m = ");                
+            } while (!int.TryParse(Console.ReadLine(), out m)||m<=0);
+
+            do
+            {
+                Console.Write("n = ");
+            } while (!int.TryParse(Console.ReadLine(), out n)||n<=0);
+
+            int[,] array = CreateAndFillArray(m, n);
+            PrintArray(array);
+
+        }
+
+        private static int[,] CreateAndFillArray(int m,int n)
+        { 
+            int[,] array = new int[m, n];
+
+            for(int row = 0; row < m; row++)
+                for(int col = 0; col < n; col++)
+                {
+                    if (row == 0 && col == 0)
+                    {
+                        array[row, col] = 0;
+                        continue;
+                    }                     
+
+                    if(row == 0 || col == 0)
+                    {
+                        array[row, col] = 1;
+                        continue;
+                    }
+
+                    array[row, col] = array[row - 1, col] + array[row, col - 1];
+
+                }
+
+            return array;
+        }
+
+        private static void PrintArray(int[,] array)
+        {
+            Console.WriteLine();
+            for (int row = 0; row < array.GetLength(0); row++)
+            {
+                for (int col = 0; col < array.GetLength(1); col++)
+                {
+                    Console.Write(String.Format("{0,5}",array[row,col]));
+                }
+                Console.WriteLine();
+            }            
         }
     }
 }
